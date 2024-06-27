76c8bec91c379efa64a86031a05a02279e29449f
Scott Klakken <scott@northernpacific.tech>
2024-06-27 12:56:10 -0700

    Switching from MySQL to Postgres. MRS (MySQL Rest Services) are incomplete at this time

--- docker/mysql-shell/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-printenv | grep 'MYSQL' > /etc/environment 
-touch /var/log/cron.log
-/usr/sbin/crond -m off && tail -f /var/log/cron.log
--- utils/bat/remove-cluster-config.bat
+++
@@ -1,3 +0,0 @@
-del ..\..\data\server-1\mysqld-auto.cnf
-del ..\..\data\server-2\mysqld-auto.cnf
-del ..\..\data\server-3\mysqld-auto.cnf
